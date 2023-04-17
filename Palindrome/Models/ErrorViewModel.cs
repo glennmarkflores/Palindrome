699b34c771101284eafb8918f0348c46c872457c
GLENN MARK FLORES <floresglennmark546@gmail.com>
2023-04-17 18:37:54 +0800

    Add project files.

---
+++ Palindrome/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Palindrome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
